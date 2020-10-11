3272479167325a786243e76d2e9d45d1b2f169e0
Дмитрий Богданов <bogdanov_s69@mail.ru>
2020-10-11 19:09:07 +0300

    Add second class for bild dictionary

---
+++ MyDictionary/MyDictionaryClass_2.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace MyDictionary
+{
+    class MyDictionaryClass_2<T, V> : IDictionary<T, V>
+    {
+        private List<int> keys;
+        private List<Entry> values;
+        private int capacity;
+
+        private struct Entry
+        {
+            public int hashCode;
+            public int next;
+            public T key;
+            public V value;
+        }
+        public MyDictionaryClass_2() : this(4)
+        {
+        }
+        public MyDictionaryClass_2(int capacity)
+        {
+            this.capacity = capacity;
+            keys = new List<int>(capacity);
+            values = new List<Entry>(capacity);
+            for (int i = 0; i < capacity; i++)
+            {
+                keys.Add(-1);
+            }
+        }
+
+        public V this[T key] { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public ICollection<T> Keys => throw new NotImplementedException();
+
+        public ICollection<V> Values => throw new NotImplementedException();
+
+        public int Count => throw new NotImplementedException();
+
+        public bool IsReadOnly => throw new NotImplementedException();
+
+        public void Add(T key, V value)
+        {
+            int index = key.GetHashCode() % capacity;
+            keys[index] = key.GetHashCode() % capacity;
+            values.Add(new Entry() {hashCode = key.GetHashCode() % capacity, key = key,next = keys[index], value = value });
+        }
+
+        public void Add(KeyValuePair<T, V> item) => this.Add(item.Key, item.Value);
+
+        public void Clear()
+        {
+            keys.Clear();
+            for (int i = 0; i < capacity; i++)
+            {
+                keys.Add(-1);
+            }
+            values.Clear();
+        }
+
+        public bool Contains(KeyValuePair<T, V> item) => ContainsKey(item.Key);
+
+        public bool ContainsKey(T key)
+        {
+            int index = key.GetHashCode() % capacity;
+            if (keys[index] != -1)
+            {
+                for (int index1 = index; index1 >= 0; index = values[index].next)
+                {
+                    if (keys[index] != -1)
+                    {
+                        return true;
+                    }
+                    //if (this.entries[index].hashCode == num && this.Comparer.Equals(this.entries[index].key, key))
+                    //    return true;
+                }
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public void CopyTo(KeyValuePair<T, V>[] array, int arrayIndex)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerator<KeyValuePair<T, V>> GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(T key)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Remove(KeyValuePair<T, V> item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool TryGetValue(T key, [MaybeNullWhen(false)] out V value)
+        {
+            throw new NotImplementedException();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- MyDictionary/Program.cs
+++ MyDictionary/Program.cs
@@ -7,19 +7,26 @@ namespace MyDictionary
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            var mydictonary = new MyDictionaryClass<int, string>();
-            mydictonary[0] = "zero";
-            mydictonary[1] = "one";
-            mydictonary.Add(2, "two");
-            Console.WriteLine(mydictonary.Count);
-            mydictonary.Clear();
-            mydictonary.Add(2, "two");
-            mydictonary[0] = "zero";
-            mydictonary[1] = "one";
-            mydictonary[2] = "two";
-            mydictonary.Remove(2);
-            Console.WriteLine(mydictonary.Count);
-            Console.WriteLine(mydictonary[2]);
+            //var mydictonary = new MyDictionaryClass<int, string>();
+            //mydictonary[0] = "zero";
+            //mydictonary[1] = "one";
+            //mydictonary.Add(2, "two");
+            //Console.WriteLine(mydictonary.Count);
+            //mydictonary.Clear();
+            //mydictonary.Add(2, "two");
+            //mydictonary[0] = "zero";
+            //mydictonary[1] = "one";
+            //mydictonary[2] = "two";
+            //mydictonary.Remove(2);
+            //Console.WriteLine(mydictonary.Count);
+            //Console.WriteLine(mydictonary[2]);
+
+            var myDictonary2 = new MyDictionaryClass_2<int, string>();
+            myDictonary2.Add(1, "one");
+            myDictonary2.Add(2, "two");
+            myDictonary2.Add(3, "three");
+            Console.WriteLine(myDictonary2.ContainsKey(1));
+            Console.WriteLine(myDictonary2.ContainsKey(4));
             Console.ReadKey();
         }
     }
